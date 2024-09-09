c538e12f8596a808aabb070b09c64a2ea517a49d
Anna <32278291+annasudol@users.noreply.github.com>
2024-09-09 14:34:45 +0200

    added VChart

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import ganttastic from '@infectoone/vue-ganttastic'
 
 const app = createApp(App)
 
 app.use(createPinia())
-
+app.use(ganttastic)
 app.mount('#app')
