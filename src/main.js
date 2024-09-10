4ea574229234a21fd149306f915ee0d1f12662be
Anna <32278291+annasudol@users.noreply.github.com>
2024-09-10 10:21:27 +0200

    removed pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import ganttastic from '@infectoone/vue-ganttastic'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(ganttastic)
 app.mount('#app')
